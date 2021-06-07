ac207c98945e83d1bf94a8cd7e74e0a811edc970
makurincamille@gmail.com <ypiemEENFtMEjme5Pq3j>
2021-06-07 23:27:18 +0300

    initial

---
+++ src/test/java/com/fridgemanager/fridgemanager/FridgemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridgemanager.fridgemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
